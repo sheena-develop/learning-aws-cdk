e510e1c995700a2db91e19b495d215b6d2679fd6
sheena-develop <sheena.develop@gmail.com>
2024-10-01 18:56:41 +0900

    fix: update pipeline.

--- cdk-workshop/lib/pipeline-stack.ts
+++ cdk-workshop/lib/pipeline-stack.ts
@@ -41,7 +41,7 @@ export class WorkshopPipelineStack extends cdk.Stack {
           'npm run build',
           'npx cdk synth',
         ],
-        primaryOutputDirectory: 'workshop-cdk/cdk/cdk.out',
+        primaryOutputDirectory: 'cdk-workshop/cdk.out',
       }),
     });
   }
