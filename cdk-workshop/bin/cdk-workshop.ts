47e18f1cb926d985a61f0246382c6cc860b898d3
sheena-develop <sheena.develop@gmail.com>
2024-10-01 17:39:37 +0900

    add cdk-workshop

---
+++ cdk-workshop/bin/cdk-workshop.ts
@@ -0,0 +1,5 @@
+import * as cdk from 'aws-cdk-lib';
+import { WorkshopPipelineStack } from '../lib/pipeline-stack';
+
+const app = new cdk.App();
+new WorkshopPipelineStack(app, 'CdkWorkshopPipelineStack');
