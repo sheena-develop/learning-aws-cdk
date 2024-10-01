5c858bef0a6dd1707bd6f2b43ad07f1ce076a852
sheena-develop <sheena.develop@gmail.com>
2024-10-01 18:32:12 +0900

    fix: update pipeline.

--- cdk-workshop/lib/pipeline-stack.ts
+++ cdk-workshop/lib/pipeline-stack.ts
@@ -1,16 +1,47 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Repository } from "aws-cdk-lib/aws-codecommit";
-import { Construct } from "constructs";
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+} from 'aws-cdk-lib/pipelines';
+import { Pipeline } from 'aws-cdk-lib/aws-codepipeline';
 
-export class WorkshopPipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class WorkshopPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Creates a CodeCommit repository called 'WorkshopRepo'
-    const repo = new Repository(this, "WorkshopRepo", {
-      repositoryName: "WorkshopRepo",
-    });
+    /* 必要な情報をコマンドライン引数経由で取得 */
+    const owner = this.node.tryGetContext('Owner') as string
+    const repository = this.node.tryGetContext('Repository') as string
+    const branch = this.node.tryGetContext('Branch') as string
+    const connectionArn = this.node.tryGetContext('ConnectionArn') as string
 
-    // Pipeline code goes here
+    // The basic pipeline declaration. This sets the initial structure
+    // of our pipeline
+    const pipeline = new CodePipeline(this, 'CodePipeline', {
+      codePipeline: new Pipeline(this, 'Pipeline', {
+        restartExecutionOnUpdate: false,
+      }),
+      selfMutation: false,
+      synth: new CodeBuildStep('SynthStep', {
+        /* GitHubから読み込み */
+        input: CodePipelineSource.connection(
+          `${owner}/${repository}`,
+          `${branch}`,
+          {
+            connectionArn: connectionArn,
+            triggerOnPush: false
+          }
+        ),
+        installCommands: ['npm install -g aws-cdk'],
+        commands: [
+          'cd cdk-workshop',
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });
   }
 }
